1f0e6dec13c054ba40189a2b11849df84fa2fe5b
Tristan Strange <tristan.strange@gmail.com>
2013-08-01 00:32:45 +0100

    Removed old menu entries

--- uk.tristanstrange.NifflasRender.xrnx/main.lua
+++ uk.tristanstrange.NifflasRender.xrnx/main.lua
@@ -57,33 +57,3 @@ renoise.tool():add_menu_entry {
 	name = "Main Menu:Tools:Nifflas Render...",
 	invoke = perform_default_render
 }
-
-renoise.tool():add_menu_entry {
-  name = "Main Menu:Tools:Nifflas Render:Song...",
-  invoke = render_song 
-}
-
-renoise.tool():add_menu_entry {
-  name = "Main Menu:Tools:Nifflas Render:Song as loop...",
-  invoke = render_song_as_loop 
-}
-
-renoise.tool():add_menu_entry {
-  name = "Main Menu:Tools:Nifflas Render:Labelled patterns...",
-  invoke = render_each_pattern 
-}
-
-renoise.tool():add_menu_entry {
-	name = "Main Menu:Tools:Nifflas Render:Labelled pattern pairs as loops...",
-	invoke = render_pattern_pairs_as_loop 
-}
-
-renoise.tool():add_menu_entry {
-  name = "Main Menu:Tools:Nifflas Render:Ungrouped tracks...",
-	invoke = render_each_track_group 
-}
-
-renoise.tool():add_menu_entry {
-	name = "Main Menu:Tools:Nifflas Render:Ungrouped tracks as loops...",
-	invoke = render_each_track_group_as_loop 
-}
